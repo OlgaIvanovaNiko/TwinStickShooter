ce656dfdc4f15e31a1b282a4a69b069f674ed065
OlgaIvanovaNiko <goliv002@edu.xamk.fi>
2024-12-13 19:17:54 +0200

    All project files added

---
+++ Source/TwinStickShooter2/TwinStickShooter2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TwinStickShooter2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickShooter2, "TwinStickShooter2" );
